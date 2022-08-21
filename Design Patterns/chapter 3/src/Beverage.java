29834d9b92e3cebc23f8cbef78817a016e5a530f
mohamedhassan218 <m.hassan.def@gmail.com>
2022-08-21 20:28:20 +0200

    Adding the rest of chapter 3, explain the enum

--- Design Patterns/chapter 3/src/Beverage.java
+++ Design Patterns/chapter 3/src/Beverage.java
@@ -1,11 +1,35 @@
 //The component class in the decorator pattern.
 public abstract class Beverage 
 {
+	public enum Size{TALL, GRANDE, VENTI};
+	
+	/*To understand the enum:
+	 * The code above is equal to this Class:
+	 * class Size
+	 * {	
+	 * 		public static final Size TALL = new Size();
+	 * 		public static final Size GRANDE = new Size();
+	 * 		public static final Size VENTI = new Size();
+	 * }
+	 * */
+	
+	Size size = Size.TALL;
+	
 	String description = "Unknown Beverage";
 	
 	public String getDescription()
 	{
 		return description;
+	}
+
+	public void setSize(Size size)
+	{
+		this.size = size;
+	}
+	
+	public Size getSize()
+	{
+		return this.size;
 	}
 	
 	public abstract double cost();
--- Design Patterns/chapter 3/src/TestDecoratorPattern.java
+++ Design Patterns/chapter 3/src/TestDecoratorPattern.java
@@ -6,14 +6,17 @@ public class TestDecoratorPattern
 	{
 		Beverage beverage = new Espresso();
 		System.out.println(beverage.getDescription() + " $" + beverage.cost());
+		//Prints: Espresso $1.99
 		
 		beverage = new Mocha(beverage);
 		System.out.println(beverage.getDescription() + " $" + beverage.cost());
+		//Prints: Espresso, Mocha $2.19
 		
 		beverage = new Whip(beverage);
 		beverage = new Mocha(beverage);
 		System.out.println(beverage.getDescription() + " $" + beverage.cost());
-
+		//Prints: Espresso, Mocha, Whip, Mocha $2.49
+		
 	}
 
 }
